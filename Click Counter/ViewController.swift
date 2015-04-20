e92293c2c87001a9b8d1a426146403173de9d243
Ruben Robles <ruben@Air-Ruben.home>
2015-04-19 22:55:44 -0700

    Up to beginning of Lesson 1:  Outlets & Actions.  Will wipe out all programmatic view code from viewcontroler.swift, then move on to storyboard scenes

--- Click Counter/ViewController.swift
+++ Click Counter/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
     var count = 0
     var label:UILabel!
+    var label2:UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +23,14 @@ class ViewController: UIViewController {
         self.view.addSubview(label)
         self.label = label
         
+        //Label 2
+        var label2 = UILabel()
+        label2.frame = CGRectMake(200, 150, 60, 60)
+        
+        label2.text = "0"
+        self.view.addSubview(label2)
+        self.label2 = label2
+        
         //Button
         var button = UIButton()
         button.frame = CGRectMake(150, 250, 60, 60)
@@ -31,12 +40,31 @@ class ViewController: UIViewController {
         
         button.addTarget(self, action: "incrementCount", forControlEvents: UIControlEvents.TouchUpInside)
         
+        //Button 2
+        var button2 = UIButton()
+        button2.frame = CGRectMake(250, 250, 60, 60)
+        button2.setTitle("Decrement", forState: .Normal)
+        button2.setTitleColor(UIColor.redColor(), forState: .Normal)
+        self.view.addSubview(button2)
+        button2.addTarget(self, action: "decrementCount", forControlEvents: UIControlEvents.TouchUpInside)
+        
+        
     }
     func incrementCount(){
         self.count++
         self.label.text = "\(self.count)"
+        self.label2.text = "\(self.count)"
+        self.view.backgroundColor = UIColor.yellowColor()
     }
     
+    func decrementCount() {
+        self.count--
+        self.label.text = "\(self.count)"
+        self.label2.text = "\(self.count)"
+        self.view.backgroundColor = UIColor.greenColor()
+        
+        
+    }
     
     
 
